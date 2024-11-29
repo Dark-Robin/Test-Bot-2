aa3b1c8622f834ffa0aa293aa8a8ece2099ddd07
Dark-Robin <sihileld23@gmail.com>
2024-11-29 21:01:25 +0530

    ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "bJU1jBiQ#KP-hqrZsgXF58DW6xWTJ1idGoYpbNGqfwUibORZ2fsc",
+MONGODB: process.env.MONGODB || "mongodb://mongo:JswNnbrJCYPIyavJMeRtUrnXETGFzJGp@junction.proxy.rlwy.net:25162",
+};
