afe9475491b7bcd2f6cfbea9b7357bf3130da62e
Dark-Robin <sihileld23@gmail.com>
2024-11-29 21:04:25 +0530

    ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️

---
+++ plugins/song.js
@@ -0,0 +1,88 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+
+
+cmd({
+    pattern: "song",
+    desc: "Download Song",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("නමක් හරි ලින්ක් එකක් හරි දෙන්න 🌚❤️")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+*❤️ROBIN Song Downloader❤️*
+
+👻 *title* : ${data.title}
+👻 *description* : ${data.description}
+👻 *time* : ${data.timestamp}
+👻 *ago* : ${data.ago}
+👻 *views* : ${data.views}
+
+𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from, {document: { url: downloadUrl },mimetype: "audio/mpeg",fileName: data.title + ".mp3",caption: "𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋"},{quoted:mek})
+
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
+
+//video download
+
+cmd({
+    pattern: "video",
+    desc: "Download Video",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("නමක් හරි ලින්ක් එකක් හරි දෙන්න 🌚❤️")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+*❤️ROBIN VIDEO DOWNLOADER❤️*
+
+👻 *title* : ${data.title}
+👻 *description* : ${data.description}
+👻 *time* : ${data.timestamp}
+👻 *ago* : ${data.ago}
+👻 *views* : ${data.views}
+
+𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from, {document: { url: downloadUrl },mimetype: "video/mp4",fileName: data.title + ".mp4",caption: "𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋"},{quoted:mek})
+
+}catch(e){
+  console.log(e)
+  reply(`${e}`)
+}
+})
