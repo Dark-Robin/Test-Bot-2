a7dcc2e7bfc32bf6ad3563b7b4bf3add2b49a2fd
Dark-Robin <sihileld23@gmail.com>
2024-11-30 00:33:03 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -18,19 +18,3 @@ reply(`${e}`)
 })
 
 
-cmd({
-    pattern: "owner",
-    alias: ["sihilel"],
-    desc: "Contact bot creater",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("නමක් හරි ලින්ක් එකක් හරි දෙන්න 🌚❤️")
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
