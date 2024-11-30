e5d37818ce1b6044c87fafdda5ead66d02050cc4
Dark-Robin <sihileld23@gmail.com>
2024-11-30 11:01:32 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,7 +11,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 const config = await readEnv();
 await conn.sendPresenceUpdate('recording', from);
-await conn.sendMessage(from, { audio: { url: "./robinMedia/autovoice/Hiruu s.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await conn.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/Hiruu%20s.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
@@ -33,7 +33,7 @@ try{
 const config = await readEnv();
 let madeOwner = `*𝐀𝐡𝐡 𝐡𝐚 𝐲𝐨𝐮 𝐮𝐬𝐞𝐝 𝐨𝐰𝐧𝐞𝐫 𝐜𝐨𝐦𝐦𝐚𝐧𝐝* 😘\n\n *𝐌𝐲 𝐍𝐚𝐦𝐞 𝐢𝐬 𝐚 𝐑_𝐎_𝐁_𝐈_𝐍* ⚡\n\n *𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐢𝐬 𝐒𝐈𝐇𝐈𝐋𝐄𝐋* 💀\n\n *𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐮𝐦𝐛𝐞𝐫 𝐥𝐢𝐧𝐤*\n\n *wa.me/+94704101989*\n\n*𝐘𝐨𝐮 𝐜𝐚𝐧 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐰𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐠𝐫𝐨𝐮𝐩 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭*\n\n*https://chat.whatsapp.com/CGQkarVNtYH2WIED9DpYSr*\n\n*𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐨𝐭𝐞* :-\n\n*𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐭𝐡𝐚𝐧𝐤𝐬 𝐭𝐨 𝐦𝐲 𝐭𝐰𝐨 𝐬𝐢𝐬𝐭𝐞𝐫𝐬 𝐋𝐀𝐊𝐒𝐇𝐈 & 𝐑𝐔𝐊𝐒𝐇𝐀𝐍𝐈* ❤️❤️`
 await conn.sendPresenceUpdate('recording', from);
-await conn.sendMessage(from, { audio: { url: "./robinMedia/autovoice/Owner.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await conn.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/Owner.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 await conn.sendMessage(from,{image: {url: "./robinMedia/robin.jpg"},caption:madeOwner},{quoted: mek})
 }catch(e){
 console.log(e)
@@ -54,7 +54,7 @@ try{
 const config = await readEnv();
 let madeOwner = `*𝐀𝐡𝐡 𝐡𝐚 𝐲𝐨𝐮 𝐮𝐬𝐞𝐝 𝐨𝐰𝐧𝐞𝐫 𝐜𝐨𝐦𝐦𝐚𝐧𝐝* 😘\n\n *𝐌𝐲 𝐍𝐚𝐦𝐞 𝐢𝐬 𝐚 𝐑_𝐎_𝐁_𝐈_𝐍* ⚡\n\n *𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐢𝐬 𝐒𝐈𝐇𝐈𝐋𝐄𝐋* 💀\n\n *𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐮𝐦𝐛𝐞𝐫 𝐥𝐢𝐧𝐤*\n\n *wa.me/+94704101989*\n\n*𝐘𝐨𝐮 𝐜𝐚𝐧 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐰𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐠𝐫𝐨𝐮𝐩 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭*\n\n*https://chat.whatsapp.com/CGQkarVNtYH2WIED9DpYSr*\n\n*𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐨𝐭𝐞* :-\n\n*𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐭𝐡𝐚𝐧𝐤𝐬 𝐭𝐨 𝐦𝐲 𝐭𝐰𝐨 𝐬𝐢𝐬𝐭𝐞𝐫𝐬 𝐋𝐀𝐊𝐒𝐇𝐈 & 𝐑𝐔𝐊𝐒𝐇𝐀𝐍𝐈* ❤️❤️`
 await conn.sendPresenceUpdate('recording', from);
-await conn.sendMessage(from, { audio: { url: "./robinMedia/autovoice/Sihilel.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await conn.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/Sihilel.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 await conn.sendMessage(from,{image: {url: "./robinMedia/robin.jpg"},caption:madeOwner},{quoted: mek})
 }catch(e){
 console.log(e)
@@ -75,7 +75,7 @@ try{
 const config = await readEnv();
 let madeOwner = `*𝐀𝐡𝐡 𝐡𝐚 𝐲𝐨𝐮 𝐮𝐬𝐞𝐝 𝐨𝐰𝐧𝐞𝐫 𝐜𝐨𝐦𝐦𝐚𝐧𝐝* 😘\n\n *𝐌𝐲 𝐍𝐚𝐦𝐞 𝐢𝐬 𝐚 𝐑_𝐎_𝐁_𝐈_𝐍* ⚡\n\n *𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐢𝐬 𝐒𝐈𝐇𝐈𝐋𝐄𝐋* 💀\n\n *𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐮𝐦𝐛𝐞𝐫 𝐥𝐢𝐧𝐤*\n\n *wa.me/+94704101989*\n\n*𝐘𝐨𝐮 𝐜𝐚𝐧 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐰𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐠𝐫𝐨𝐮𝐩 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭*\n\n*https://chat.whatsapp.com/CGQkarVNtYH2WIED9DpYSr*\n\n*𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐨𝐭𝐞* :-\n\n*𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐭𝐡𝐚𝐧𝐤𝐬 𝐭𝐨 𝐦𝐲 𝐭𝐰𝐨 𝐬𝐢𝐬𝐭𝐞𝐫𝐬 𝐋𝐀𝐊𝐒𝐇𝐈 & 𝐑𝐔𝐊𝐒𝐇𝐀𝐍𝐈* ❤️❤️`
 await conn.sendPresenceUpdate('recording', from);
-await conn.sendMessage(from, { audio: { url: "./robinMedia/autovoice/I love you.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await conn.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/I%20love%20you.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 await conn.sendMessage(from,{image: {url: "./robinMedia/robin.jpg"},caption:madeOwner},{quoted: mek})
 }catch(e){
 console.log(e)
@@ -96,7 +96,7 @@ try{
 const config = await readEnv();
 let madeOwner = `*𝐀𝐡𝐡 𝐡𝐚 𝐲𝐨𝐮 𝐮𝐬𝐞𝐝 𝐨𝐰𝐧𝐞𝐫 𝐜𝐨𝐦𝐦𝐚𝐧𝐝* 😘\n\n *𝐌𝐲 𝐍𝐚𝐦𝐞 𝐢𝐬 𝐚 𝐑_𝐎_𝐁_𝐈_𝐍* ⚡\n\n *𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐢𝐬 𝐒𝐈𝐇𝐈𝐋𝐄𝐋* 💀\n\n *𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐮𝐦𝐛𝐞𝐫 𝐥𝐢𝐧𝐤*\n\n *wa.me/+94704101989*\n\n*𝐘𝐨𝐮 𝐜𝐚𝐧 𝐣𝐨𝐢𝐧 𝐨𝐮𝐫 𝐰𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐠𝐫𝐨𝐮𝐩 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐛𝐨𝐭*\n\n*https://chat.whatsapp.com/CGQkarVNtYH2WIED9DpYSr*\n\n*𝐂𝐫𝐞𝐚𝐭𝐞𝐫 𝐧𝐨𝐭𝐞* :-\n\n*𝐒𝐩𝐞𝐜𝐢𝐚𝐥 𝐭𝐡𝐚𝐧𝐤𝐬 𝐭𝐨 𝐦𝐲 𝐭𝐰𝐨 𝐬𝐢𝐬𝐭𝐞𝐫𝐬 𝐋𝐀𝐊𝐒𝐇𝐈 & 𝐑𝐔𝐊𝐒𝐇𝐀𝐍𝐈* ❤️❤️`
 await conn.sendPresenceUpdate('recording', from);
-await conn.sendMessage(from, { audio: { url: "./robinMedia/autovoice/I love you.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await conn.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/I%20love%20you.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 await conn.sendMessage(from,{image: {url: "./robinMedia/robin.jpg"},caption:madeOwner},{quoted: mek})
 }catch(e){
 console.log(e)
