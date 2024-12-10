ee639b0662a79a34a01fed28b09791abd5b0ba54
Dark-Robin <sihileld23@gmail.com>
2024-12-10 12:55:55 +0530

    Create fun.js

---
+++ plugins/fun.js
@@ -0,0 +1,14 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+
+  
