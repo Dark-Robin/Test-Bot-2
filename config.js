47cf1f53aae12710c34c047f04e7843adaac0ab4
Dark-Robin <sihileld23@gmail.com>
2024-12-13 22:56:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rUsUCLaS#QJ1W3ioNPS8qACuBOqsKcPhqy31M6COdlyFryzCOFoo",
+SESSION_ID: process.env.SESSION_ID || "rU8RjCCb#I6o_oyty65phnfUIKHUec437iLVfJZa37nHhv8-X424",
 MONGODB: process.env.MONGODB || "mongodb://mongo:QfpiXGuAfauZGAIcOVcclWHAyvoqZPFX@junction.proxy.rlwy.net:32111",
 };
